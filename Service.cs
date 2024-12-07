e4268cc9feaf209c807c528b6e26e3c1aa3f1fe8
Yachunee <Goite.220440@gensan.sti.edu.ph>
2024-12-08 03:29:00 +0800

    Service Form no Functions

---
+++ Service.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mini_Cs
+{
+    public partial class Service : Form
+    {
+        public Service()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBoxDmOthers_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbViewingPlace_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuDatePicker2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBoxHearse_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
